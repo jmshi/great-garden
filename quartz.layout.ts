75538b973b8cade98e9487d7639cab2839cb07ac
jmshi <jmshi@users.noreply.github.com>
2025-12-07 14:27:50 -0500

    layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -47,7 +47,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.MobileOnly(Component.Explorer()),
-    Component.Backlinks(),
+    Component.DesktopOnly(Component.Backlinks()),
   ],
   // pageSpecificLayouts removed from here
 }
